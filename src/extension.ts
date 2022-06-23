e6f00d24a565aa49d600666a7f5567776e4df16a
aagZTo <tobias.zaugg@ammann.com>
2022-06-23 13:14:59 +0200

    keep cursor position in real file -> switched to onDidChangeTextEditorSelection

--- src/extension.ts
+++ src/extension.ts
@@ -2,66 +2,77 @@ import * as vscode from 'vscode';
 import * as fs from 'fs';
 
 export function activate(context: vscode.ExtensionContext) {
-	let lastRealFilePath = '';
+  let lastRealFilePath = '';
 
-	const disposable = vscode.window.onDidChangeActiveTextEditor(async ed => {
-		if (!ed || ed.document.uri.scheme !== 'file') return;
-		const filePath = ed.document.fileName!;
+  const disposable = vscode.window.onDidChangeTextEditorSelection(async (selection) => {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor || editor.document.uri.scheme !== 'file') return;
+    const filePath = editor.document.fileName!;
 
-		// Skipping the file we just opened.
-		if (filePath === lastRealFilePath) return;
+    // Skipping the file we just opened.
+    if (filePath === lastRealFilePath) return;
 
-		fs.realpath(filePath, async (err, realFilePath) => {
-			if (err) {
-				// No need to burden user with error
-				return;
-			}
+    // get current selection
+    const targetSelection = selection.selections[0];
 
-			// Not a symlink. No need to open real path.
-			if (filePath === realFilePath) return;
-			const symlinkFollowConfig = vscode.workspace.getConfiguration('symlink-follow');
-			const onlyFollowWithinWorkspace = symlinkFollowConfig.get('onlyFollowWithinWorkspace');
-			const realFileUri = vscode.Uri.file(realFilePath);
+    fs.realpath(filePath, async (err, realFilePath) => {
+      if (err) {
+        // No need to burden user with error
+        return;
+      }
 
-			if (onlyFollowWithinWorkspace) {
-				if (!vscode.workspace.getWorkspaceFolder(realFileUri)) {
-					// Our file isn't within any workspace folder and the user has asked
-					// to only follow symlink files within the workspace folder(s). Skip!
-					return;
-				}
-			}
+      // Not a symlink. No need to open real path.
+      if (filePath === realFilePath) return;
+      const symlinkFollowConfig = vscode.workspace.getConfiguration('symlink-follow');
+      const onlyFollowWithinWorkspace = symlinkFollowConfig.get('onlyFollowWithinWorkspace');
+      const realFileUri = vscode.Uri.file(realFilePath);
 
-			const showFileInExplorer = symlinkFollowConfig.get('showFileInExplorerAfterSymlinkFollow');
-			const followSymlink = async () => {
-				lastRealFilePath = realFilePath;
-				if (vscode.window.activeTextEditor?.document.fileName == filePath) {
-					vscode.commands.executeCommand('workbench.action.closeActiveEditor');
-				}
-				await vscode.commands.executeCommand('vscode.open', realFileUri);
-				if (showFileInExplorer) {
-					await vscode.commands.executeCommand('workbench.files.action.showActiveFileInExplorer');
-				}
-			};
+      if (onlyFollowWithinWorkspace) {
+        if (!vscode.workspace.getWorkspaceFolder(realFileUri)) {
+          // Our file isn't within any workspace folder and the user has asked
+          // to only follow symlink files within the workspace folder(s). Skip!
+          return;
+        }
+      }
 
-			const autoFollow = symlinkFollowConfig.get('autoFollow');
-			if (!autoFollow) {
-				const item = await vscode.window.showInformationMessage(
-					`${vscode.workspace.asRelativePath(filePath)} is a symlink`,
-					{ modal: true, detail: `The real path is ${vscode.workspace.asRelativePath(realFilePath)}` },
-					'Follow Symlink',
-				);
+      const showFileInExplorer = symlinkFollowConfig.get('showFileInExplorerAfterSymlinkFollow');
+      const followSymlink = async () => {
+        lastRealFilePath = realFilePath;
+        if (vscode.window.activeTextEditor?.document.fileName == filePath) {
+          vscode.commands.executeCommand('workbench.action.closeActiveEditor');
+        }
+        await vscode.commands.executeCommand('vscode.open', realFileUri);
 
-				if (item) {
-					followSymlink();
-				}
-			} else {
-				followSymlink();
-			}
-		});
+        // set selection
+        const newEditor = vscode.window.activeTextEditor;
+        if (newEditor) {
+          newEditor.selection = targetSelection;
+        }
 
-	});
+        if (showFileInExplorer) {
+          await vscode.commands.executeCommand('workbench.files.action.showActiveFileInExplorer');
+        }
+      };
 
-	context.subscriptions.push(disposable);
+      const autoFollow = symlinkFollowConfig.get('autoFollow');
+      if (!autoFollow) {
+        const item = await vscode.window.showInformationMessage(
+          `${vscode.workspace.asRelativePath(filePath)} is a symlink`,
+          { modal: true, detail: `The real path is ${vscode.workspace.asRelativePath(realFilePath)}` },
+          'Follow Symlink',
+        );
+
+        if (item) {
+          followSymlink();
+        }
+      } else {
+        followSymlink();
+      }
+    });
+
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
